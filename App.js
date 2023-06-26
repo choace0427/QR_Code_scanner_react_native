80aca31f77d89f306d1c83fb94f37e3b8b84feb8
choace0427 <jacobkevin0427@gmail.com>
2023-06-26 22:18:22 +0330

    ask permissions

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {BarCodeScanner} from 'expo-barcode-scanner';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText]  = useState("Not Yet Scanned");
+  const [totalScan, setTotalScan] = useState(0);
+
+  const askForCameraPermission = ()=>{
+    (async ()=>{
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == 'Granted')
+    })()
+  }
+
+  // Request Camera Permission
+  useEffect(()=>{
+    askForCameraPermission();
+  },[])
+
+  // What Happens when we scan the bar code
+  const handleBarCodeScanned = ({type, data}) =>{
+    setScanned(True);
+    setText(data);
+    console.log('Type: ', type , ' Data: ', data);
+  }
+
+  // check permissions and return the screens
+  if (hasPermission === null){
+    return(
+      <View style={styles.container}>
+        <Text>Requesting For Camera Permission</Text>
+      </View>
+    )
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yooooooooo</Text>
       <StatusBar style="auto" />
     </View>
   );
